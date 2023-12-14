d8e33d8d64d3cf879fe8d4c2e44182d73a1cca1d
aletardio <ale.tardio99@gmail.com>
2023-12-14 19:27:56 +0100

    Added messages for every user in chat

--- js/script.js
+++ js/script.js
@@ -5,7 +5,21 @@ createApp ({
         return{
             name:'Michele',
             avatar: './img/avatar_1.png',
-            user_messages:'',
+            user_messages: [{
+                date: '10/01/2020 15:30:55',
+                message: 'Hai portato a spasso il cane?',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 15:50:00',
+                message: 'Ricordati di stendere i panni',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 16:15:22',
+                message: 'Tutto fatto!',
+                status: 'received'
+              }],
             contacts: [
                 {
                   name: 'Michele',
